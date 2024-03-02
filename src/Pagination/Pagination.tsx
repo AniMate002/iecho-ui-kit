6598f3d0727780de3eafe7634a29d5e7bc08b3d3
Alukard <kirill1974111@gmail.com>
2024-03-02 19:37:43 +0100

    small fixes

--- src/Pagination/Pagination.tsx
+++ src/Pagination/Pagination.tsx
@@ -1,7 +1,6 @@
 import React, { DetailedHTMLProps, HTMLAttributes, ReactElement, ReactNode } from "react";
 import PaginationNavBtn from "./PaginationNavBtn";
 import './Pagination.css'
-import PaginationNavNext from "./PaginationNavNext";
 
 export interface PaginationProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>{
     children?: ReactNode[],
@@ -12,7 +11,6 @@ const Pagination:React.FC<PaginationProps> = ({ children, clasName, ... props}) 
     return (
         <nav className={`Navigation ${clasName ? clasName : ''}`} {...props}>
             {children}
-            {/* <PaginationNavNext /> */}
         </nav>
     );
 }
